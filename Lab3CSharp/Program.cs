3b35cbddfbd269bcac8e4e82759bd678c7ed4734
Venomorous <94299601+Venomorous@users.noreply.github.com>
2024-02-10 16:35:25 +0200

    Complete task1

--- Lab3CSharp/Program.cs
+++ Lab3CSharp/Program.cs
@@ -1,10 +1,95 @@
-﻿namespace Lab3CSharp
+﻿using System;
+
+class ITriangle
 {
-    internal class Program
+    protected int a;
+    protected int b;
+
+    protected int color;
+
+    public ITriangle(int a, int b, int color)
     {
-        static void Main(string[] args)
+        //if (a < 0 || b < 0)
+        //{
+        //    throw new ArgumentException("Sides must be greater than 0");
+        //}
+
+        if (!IsTriangleCorrect(a, b))
         {
-            Console.WriteLine("Lab 3 ");
+            throw new ArgumentException("Triangle is not correct");
         }
+
+        this.a = a;
+        this.b = b;
+        this.color = color;
+    }
+
+    public int A
+    {
+        get => a;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Side A must be greater than 0");
+            }
+            a = value;
+        }
+    }
+
+    public int B
+    {
+        get => b;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Side B must be greater than 0");
+            }
+            b = value;
+        }
+    }
+
+    public int Color
+    {
+        get => color;
+    }
+
+    public void printSides()
+    {
+        Console.WriteLine("Side A: " + A);
+        Console.WriteLine("Side B: " + B);
+    }
+
+    public int Perimeter()
+    {
+        return A * 2 + B;
+    }
+
+    public double Area()
+    {
+        return (B / 2) * Math.Sqrt(A * A - (B * B) / 4);
+    }
+
+    public bool IsTriangleCorrect()
+    {
+        return A + B > A && A + B > B;
+    }
+
+    private static bool IsTriangleCorrect(int a, int b)
+    {
+        return a + b > a && a + b > b && b + a > b;
+    }
+}
+
+public class Lab3
+{
+    public static void Main(string[] args)
+    {
+        ITriangle triangle = new ITriangle(3, 4, 1);
+        Console.WriteLine("Perimeter: " + triangle.Perimeter());
+        Console.WriteLine("Area: " + triangle.Area());
+        Console.WriteLine("Color: " + triangle.Color);
+        Console.WriteLine("Is triangle correct: " + triangle.IsTriangleCorrect());
     }
 }
